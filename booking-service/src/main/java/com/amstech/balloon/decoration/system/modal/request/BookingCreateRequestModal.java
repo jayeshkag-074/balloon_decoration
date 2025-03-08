57528ce34554f0f3c42ce28bd94321758624f940
Emarger <atulpatel6357@gmail.com>
2025-03-08 08:43:38 +0530

    Booking Controller Api

---
+++ booking-service/src/main/java/com/amstech/balloon/decoration/system/modal/request/BookingCreateRequestModal.java
@@ -0,0 +1,9 @@
+package com.amstech.balloon.decoration.system.modal.request;
+
+import lombok.Data;
+
+@Data
+public class BookingCreateRequestModal {
+	private int userId;
+	private int decorationId;
+}
